7ad637e8bd9ff419ce30344c5b1caf1a4b8e8737
Mark Swatosh <mark.swatosh@gmail.com>
2020-02-20 16:07:47 -0600

    Working kerberos between a WebSphere Servlet and DB2

--- src/main/java/com/example/ExampleServlet.java
+++ src/main/java/com/example/ExampleServlet.java
@@ -17,12 +17,15 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 @WebServlet("/example")
-@ServletSecurity(value = @HttpConstraint(rolesAllowed = { "Manager"},
-  transportGuarantee = ServletSecurity.TransportGuarantee.NONE))
-public class ExampleServlet extends HttpServlet { 
+@ServletSecurity(value = @HttpConstraint(rolesAllowed = {
+        "Manager" }, transportGuarantee = ServletSecurity.TransportGuarantee.NONE))
+public class ExampleServlet extends HttpServlet {
 
     @Resource(name = "jdbc/db2ds")
     DataSource ds;
+
+    @Resource(name = "jdbc/nokrb5")
+    DataSource noKrb5;
 
     private static final long serialVersionUID = 1L;
 
@@ -37,12 +40,34 @@ public class ExampleServlet extends HttpServlet {
         pw.println("getAuthType: " + request.getAuthType());
         pw.println("getRemoteUser: " + request.getRemoteUser());
         pw.println("getUserPrincipal: " + request.getUserPrincipal());
-        //pw.println("getUserPrincipal().getName(): " + request.getUserPrincipal() == null ? "User Principal is null" : request.getUserPrincipal().getName());
 
-        try (Connection con = ds.getConnection(); Statement stmt = con.createStatement();) {
-            stmt.execute("CREATE TABLE MYTABLE (ID SMALLINT NOT NULL PRIMARY KEY, STRVAL NVARCHAR(40))");
+        pw.println("<br/>Attempting non-kerberos connection<br/>");
+        try (Connection con = noKrb5.getConnection(); Statement stmt = con.createStatement();) {
+            try {
+                stmt.execute("DROP TABLE krb5test");
+            } catch (SQLException x) {
+                if (!"42704".equals(x.getSQLState()))
+                    throw x;
+            }
+            stmt.execute("CREATE TABLE krb5test (ID SMALLINT NOT NULL PRIMARY KEY, STRVAL NVARCHAR(40))");
             stmt.close();
-            pw.println("Create Table Successful\n");
+            pw.println("<br/>Create Table Successful<br/>");
+        } catch (SQLException e) {
+            pw.println("<br/>No Kerberos Datasource failed:<br/>");
+            e.printStackTrace(pw);
+        }
+
+        pw.println("<br/><br/>Attempting kerberos connection<br/>");
+        try (Connection con = ds.getConnection(); Statement stmt = con.createStatement();) {
+            try {
+                stmt.execute("DROP TABLE krb5test");
+            } catch (SQLException x) {
+                if (!"42704".equals(x.getSQLState()))
+                    throw x;
+            }
+            stmt.execute("CREATE TABLE krb5test (ID SMALLINT NOT NULL PRIMARY KEY, STRVAL NVARCHAR(40))");
+            stmt.close();
+            pw.println("<br/>Create Table Successful<br/>");
         } catch (SQLException e) {
             e.printStackTrace(pw);
         }
--- websphere-traditional/work/config/kerberos.py
+++ websphere-traditional/work/config/kerberos.py
@@ -6,9 +6,6 @@ AdminConfig.save()
 AdminTask.applyWizardSettings('[-secureApps true -secureLocalResources false -adminPassword password -userRegistryType WIMUserRegistry -adminName wsadmin ]')  
 AdminConfig.save()
 
-#Map users
-AdminTask.createUser('[-uid db2user/websphere -cn db2 -sn user -password password -confirmPassword password]')
-AdminApp.edit('was-kerberos-database_war', '[ -MapRolesToUsers [[ Manager AppDeploymentOption.No AppDeploymentOption.No wsadmin|db2user/websphere "" AppDeploymentOption.No user:defaultWIMFileBasedRealm/uid=wsadmin,o=defaultWIMFileBasedRealm|user:defaultWIMFileBasedRealm/uid=db2user/websphere,o=defaultWIMFileBasedRealm "" ]]]' )
 
 #Configure Kerberos
 AdminTask.createKrbAuthMechanism('[-krb5Realm EXAMPLE.COM -krb5Config /etc/krb5.conf -krb5Keytab /etc/krb5.keytab -serviceName db2user -trimUserName true -enabledGssCredDelegate true -allowKrbAuthForCsiInbound true -allowKrbAuthForCsiOutbound true ]') 
@@ -16,9 +13,11 @@ AdminTask.setAdminActiveSecuritySettings('[-customProperties ["com.ibm.websphere
 AdminConfig.save()
 
 #Configure Datasource
+AdminTask.createAuthDataEntry('[-alias userpass -user db2user/websphere@EXAMPLE.COM -password password -description ]') 
 providerID = AdminTask.createJDBCProvider('[-scope Cell=DefaultCell01 -databaseType DB2 -providerType "DB2 Using IBM JCC Driver" -implementationType "Connection pool data source" -name "DB2 Using IBM JCC Driver" -description "One-phase commit DB2 JCC provider that supports JDBC 4.0 using the IBM Data Server Driver for JDBC and SQLJ. IBM Data Server Driver is the next generation of the DB2 Universal JCC driver. Data sources created under this provider support only 1-phase commit processing except in the case where JDBC driver type 2 is used under WebSphere Application Server for Z/OS. On WebSphere Application Server for Z/OS, JDBC driver type 2 uses RRS and supports 2-phase commit processing. This provider is configurable in version 7.0 and later nodes." -classpath [/opt/drivers/db2jcc.jar ] -nativePath [${DB2_JCC_DRIVER_NATIVEPATH} ] ]') 
 AdminConfig.save()
 datasourceID = AdminTask.createDatasource(providerID, '[-name DB2 -jndiName jdbc/db2ds -dataStoreHelperClassName com.ibm.websphere.rsadapter.DB2UniversalDataStoreHelper -containerManagedPersistence true -componentManagedAuthenticationAlias -configureResourceProperties [[databaseName java.lang.String TESTDB] [driverType java.lang.Integer 4] [serverName java.lang.String db2] [portNumber java.lang.Integer 50000]]]') 
+AdminTask.createDatasource(providerID, '[-name NoKrb5 -jndiName jdbc/nokrb5 -dataStoreHelperClassName com.ibm.websphere.rsadapter.DB2UniversalDataStoreHelper -containerManagedPersistence false -componentManagedAuthenticationAlias DefaultNode01/userpass -configureResourceProperties [[databaseName java.lang.String TESTDB] [driverType java.lang.Integer 4] [serverName java.lang.String db2] [portNumber java.lang.Integer 50000]]]') 
 AdminConfig.create('MappingModule', datasourceID, '[[authDataAlias ""] [mappingConfigAlias KerberosMapping]]') 
 AdminConfig.save()
 #Set properties on Datasource
